add47a159b509003e5e2eb9ceab5e217e9ce0043
Anubhav Ghosh <iamanubhav11@gmail.com>
2025-05-02 21:39:54 +0530

    feat: User controller and Initial setup

---
+++ src/models/user.model.ts
@@ -0,0 +1,67 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+export interface IUser extends Document {
+    username: string;
+    email: string;
+    passwordHash: string;
+    avatarUrl?: string;
+    bio?: string;
+    displayName?: string;
+    socialLinks?: Record<string, string>;
+    theme?: {
+      color: string;
+      background: string;
+      font: string;
+    };
+}
+
+const userSchema = new Schema<IUser>({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+    },
+    avatarUrl: {
+        type: String,
+    },
+    bio: {
+        type: String,
+        maxlength: 160,
+    },
+    displayName: {
+        type: String,
+        maxlength: 50,
+    },
+    socialLinks: Object,
+    theme: {
+        color: {
+            type: String,
+            default: "#000000",
+        },
+        background: {
+            type: String,
+            default: "#FFFFFF",
+        },
+        font: {
+            type: String,
+            default: "Arial",
+        },
+        image: {
+            type: String,
+        }
+    },
+})
+
+export default mongoose.model<IUser>("User", userSchema);
