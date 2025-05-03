0c0d8fc758c498d8f34d0e02ebea976d3161641e
Anubhav Ghosh <iamanubhav11@gmail.com>
2025-05-04 00:12:28 +0530

    feat: Link Controller

--- src/controllers/link.controller.ts
+++ src/controllers/link.controller.ts
@@ -46,6 +46,10 @@ const createLink = asyncHandler(
         logo,
       });
 
+      const updateUser = await User.findByIdAndUpdate(userId, {
+        $push: { socialLinks: newLink._id },
+      });
+
       return res
         .status(201)
         .json(new ApiResponse(201, newLink, "Link created successfully"));
@@ -62,4 +66,144 @@ const createLink = asyncHandler(
   }
 );
 
-export { createLink };
+/**
+ * Handles the deletion of a link.
+ *
+ * @param req - The HTTP request object containing the link ID in the parameters.
+ * @param res - The HTTP response object used to send the response back to the client.
+ *
+ * @returns A JSON response indicating the success or failure of the link deletion process.
+ *          On success, it returns a 200 status with a success message.
+ *          On failure, it returns an appropriate error status and message.
+ */
+
+const deleteLink = asyncHandler(
+  async (req: ILinkControllerRequest, res: Response) => {
+    try {
+      const { linkId } = req.params;
+      const userId = req.user._id;
+
+      if (!linkId) {
+        return res
+          .status(400)
+          .json(new ApiResponse(400, {}, "Please provide all required fields"));
+      }
+
+      const link = await Link.findById(linkId);
+      if (!link) {
+        return res.status(404).json(new ApiResponse(404, {}, "Link not found"));
+      }
+
+      if (link.user.toString() !== userId.toString()) {
+        return res
+          .status(403)
+          .json(
+            new ApiResponse(
+              403,
+              {},
+              "You are not authorized to delete this link"
+            )
+          );
+      }
+
+      const updateUser = await User.findByIdAndUpdate(userId, {
+        $pull: { socialLinks: linkId },
+      });
+
+      return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Link deleted successfully"));
+    } catch (e) {
+      if (e instanceof ApiError) {
+        return res
+          .status(e.statusCode)
+          .json(new ApiResponse(e.statusCode, {}, e.message));
+      }
+      return res
+        .status(500)
+        .json(new ApiResponse(500, {}, "Internal server error"));
+    }
+  }
+);
+
+interface ILink {
+  url?: string;
+  title?: string;
+  logo?: string;
+}
+
+const updateLink = asyncHandler(
+  async (req: ILinkControllerRequest, res: Response) => {
+    try {
+      const { linkId } = req.params;
+      const userId = req.user._id;
+      const { url, title, logo } = req.body;
+      if (!linkId) {
+        return res
+          .status(400)
+          .json(new ApiResponse(400, {}, "Please provide all required fields"));
+      }
+
+      let updatedDetails: ILink = {};
+      if (url) {
+        updatedDetails.url = url;
+      }
+      if (title) {
+        updatedDetails = { ...updatedDetails, title };
+      }
+      if (logo) {
+        updatedDetails = { ...updatedDetails, logo };
+      }
+
+      const link = await Link.findById(linkId);
+      if (!link) {
+        return res.status(404).json(new ApiResponse(404, {}, "Link not found"));
+      }
+
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json(new ApiResponse(404, {}, "User not found"));
+      }
+
+      if (
+        Array.isArray(user.socialLinks) &&
+        user.socialLinks.includes(linkId)
+      ) {
+        const updatedLink = await Link.findByIdAndUpdate(
+          linkId,
+          { ...updatedDetails },
+          { new: true }
+        );
+        if (!updatedLink) {
+          return res
+            .status(404)
+            .json(new ApiResponse(404, {}, "Link not found"));
+        }
+        return res
+          .status(200)
+          .json(new ApiResponse(200, updatedLink, "Link updated successfully"));
+      } else {
+        return res
+          .status(403)
+          .json(
+            new ApiResponse(
+              403,
+              {},
+              "You are not authorized to update this link"
+            )
+          );
+      }
+    } catch (e) {
+      if (e instanceof ApiError) {
+        return res
+          .status(e.statusCode)
+          .json(new ApiResponse(e.statusCode, {}, e.message));
+      }
+      return res
+        .status(500)
+        .json(new ApiResponse(500, {}, "Internal server error"));
+    }
+  }
+);
+
+export { createLink, deleteLink, updateLink };
